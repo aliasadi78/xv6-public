2b458b4c4d36a193328308eb5ea9b0c8917d728e
aliasadi78 <aliasadi072@gmail.com>
2020-06-08 22:17:21 +0430

    phase1

---
+++ ps.c
@@ -0,0 +1,53 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "param.h"
+
+int
+main(void)
+{
+    int pid = fork();
+    if (pid == 0){
+        fork();
+        fork();
+        fork();
+        int pidx = getpid();
+        if (pidx % 8 == 0){
+            int *ptr = (int *)malloc(123456 * sizeof(int));
+            ptr++;
+        }
+        else if (pidx % 8 == 1){
+            int *ptr = (int *)malloc(234567 * sizeof(int));
+            ptr++;
+        }
+        else if (pidx % 8 == 2){
+            int *ptr = (int *)malloc(345678 * sizeof(int));
+            ptr++;
+        }
+        else if (pidx % 8 == 3){
+            int *ptr = (int *)malloc(456789 * sizeof(int));
+            ptr++;
+        }
+        else if (pidx % 8 == 4){
+            int *ptr = (int *)malloc(567890 * sizeof(int));
+            ptr++;
+        }
+        else if (pidx % 8 == 5){
+            int *ptr = (int *)malloc(678901 * sizeof(int));
+            ptr++;
+        }
+        else if (pidx % 8 == 6){
+            int *ptr = (int *)malloc(789012 * sizeof(int));
+            ptr++;
+        }
+        else {
+            int *ptr = (int *)malloc(890123 * sizeof(int));
+            ptr++;
+        }
+        while(1){
+        }
+    }
+    else{
+        sleep(2);
+    }
+}
