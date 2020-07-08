55e20273092fd9600354da54d241176b2c3a518a
aliasadi78 <aliasadi072@gmail.com>
2020-07-08 18:14:53 +0430

    phase2

--- proc.h
+++ proc.h
@@ -49,6 +49,11 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  int  stime;    //process start time
+  int  etime;     //process end time
+  int  iotime;    //process io time
+  int  rtime;     //process run time
 };
 
 // Process memory is laid out contiguously, low addresses first:
