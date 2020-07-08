8da29cd4e798a9bdf9fc5fd6ed07c77e1af7bdc0
aliasadi78 <aliasadi072@gmail.com>
2020-07-08 15:44:41 +0430

    phase2

--- defs.h
+++ defs.h
@@ -120,6 +120,7 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int              waitx(int *wtime, int *rtime); //function waitx(int *wtime, int *rtime) for part1
 
 // swtch.S
 void            swtch(struct context**, struct context*);
